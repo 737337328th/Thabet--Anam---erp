31128102172594bd34273b68390635fd521b58eb
Thabet Anam <thabet.anam@gmail.com>
2026-09-22 15:43:10 +0300

    Add Thabet Anam V4 production source

---
+++ thabet_anam_v4/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "module_name": "Thabet Anam V4",
+            "type": "module",
+            "label": _("ثابت أنعم V4"),
+        }
+    ]
